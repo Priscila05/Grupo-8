1e6bff756758bf621d7e94f4ce4dcc2e0e64c952
Jvesus89 <jvjesus89@gmail.com>
2024-11-22 10:53:28 -0300

    Criado a validação de login para estudante,admin, professor e usuario, criado a tela de cadastro para cada usuario, criado a tela para cadastro de faixa disponivel

--- login.js
+++ login.js
@@ -6,22 +6,38 @@ function verificarLogin() {
    if (usuario == 'admin' && senha == '123') {
    localStorage.setItem('estaLogado', 'true');
    localStorage.setItem('nomeUsuario', usuario);
-   habilitarAgendamentos();
-   FazerLogin();    
-        } else {
+   window.location.href = 'Index.html';
+   FazerLogin();  
+   return  
+        } 
+   if (usuario == 'estudante' && senha == '123'){
+      localStorage.setItem('estaLogado', 'true');
+      localStorage.setItem('nomeUsuario', usuario);
+      window.location.href = 'Index.html';
+      FazerLogin();    
+      return
+   }
+   if (usuario == 'professor' && senha == '123'){
+      localStorage.setItem('estaLogado', 'true');
+      localStorage.setItem('nomeUsuario', usuario);
+      window.location.href = 'Index.html';
+      FazerLogin();   
+      return 
+   }
+   if (usuario == 'usuario' && senha == '123') {
+    localStorage.setItem('estaLogado', 'true');
+    localStorage.setItem('nomeUsuario', usuario);
+    window.location.href = 'Index.html';
+    FazerLogin();  
+    return  
+         } 
         alert('Login inválido');
-    }
-}
-function habilitarAgendamentos() {
-   // Habilita o link para "Agendamentos"
-   document.getElementById('menu-tops-active').removeAttribute('disabled');
 }
 
    // Verificação inicial ao carregar a página
    
 window.onload = function() {
    if (localStorage.getItem('estaLogado') === 'true') {
-   habilitarAgendamentos();
    FazerLogin();  
 }  
 } 
@@ -51,5 +67,64 @@ function logout() {
    // Remove o nome do usuário do localStorage e recarrega a página
    localStorage.removeItem('nomeUsuario');
    localStorage.removeItem('estaLogado');
+   localStorage.removeItem('caminhoCadastro');
    location.reload();
+   window.location.href = 'Index.html';
    } 
+
+
+//VALIDAÇÃO PERMISÃO USUARIOS
+function verificaCaminhoCadastro() {
+    let caminho = '';
+
+    if (localStorage.getItem('estaLogado') === 'true') {
+        const nomeUsuario = localStorage.getItem('nomeUsuario');
+
+        switch (nomeUsuario) {
+            case 'admin':
+                caminho = "cadastroAdmin.html";
+                break;
+            case 'estudante':
+                caminho = "cadastroestudante.html";
+                break;
+            case 'professor':
+                caminho = "cadastroprofessor.html";
+                break;
+            default:
+                caminho = 'cadastro.html';
+        }
+    } else {
+        caminho = 'cadastro.html';
+    }
+
+localStorage.setItem('caminhoCadastro', caminho); 
+window.location.href = caminho;
+}  
+
+function verificaCaminhoagendamento() {
+    let caminho = '';
+
+    if (localStorage.getItem('estaLogado') === 'true') {
+        const nomeUsuario = localStorage.getItem('nomeUsuario');
+
+        switch (nomeUsuario) {
+            case 'admin':
+                caminho = "agendamentosAdmin.html";
+                break;
+            case 'estudante':
+                caminho = "Agendamentos.html";
+                break;
+            case 'professor':
+                caminho = "AgendamentosProfessor.html";
+                break;
+            default:
+                caminho = 'Agendamentos.html';
+        }
+    } else {
+        caminho = 'Agendamentos.html';
+    }
+
+localStorage.setItem('caminhoCadastro', caminho); 
+window.location.href = caminho;
+}  
+
